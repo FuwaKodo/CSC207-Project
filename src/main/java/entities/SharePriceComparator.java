ae188d24d83971787957a37695471f440c0440f1
Victor Gao <victorgao44@gmail.com>
2024-11-05 14:47:50 -0500

    Add comparator interface and one concrete class SharePriceComparator to compare two stocks.

---
+++ src/main/java/entities/SharePriceComparator.java
@@ -0,0 +1,8 @@
+package main.java.entities;
+
+public class SharePriceComparator implements StockComparator {
+    @Override
+    public int compare(Stock stock1, Stock stock2) {
+        return 0;
+    }
+}

f5143bd630f5379d070e1c63c767077038493884
Proedricmants <gameknight999credric@gmail.com>
2024-11-07 21:58:04 -0500

    Added api_loader documentation

--- src/main/java/api_loader_experiment.java
+++ src/main/java/api_loader_experiment.java
@@ -13,6 +13,10 @@ public class api_loader_experiment {
                             "https://api.polygon.io/v1/open-close/INTC/2024-11-06?adjusted=true&apiKey=NbKXkuoH3mdV4H6DN493zVQg0M2d0LlG",
                             "https://api.polygon.io/v1/open-close/AAPL/2024-11-06?adjusted=true&apiKey=NbKXkuoH3mdV4H6DN493zVQg0M2d0LlG"
                     };
+            /* API Endpoint Format: https://api.polygon.io/v1/open-close/COMPANY_CODE/DATE?adjusted=true&apiKey=NbKXkuoH3mdV4H6DN493zVQg0M2d0LlG
+            * COMPANY_CODE: String
+            * DATE: YYYY-MM-DD
+            * */
 
             String apiEndpoint = listOfApi[1];
 
@@ -27,8 +31,18 @@ public class api_loader_experiment {
             // Send the request and get the response
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            // Print the response body (usually JSON data)
+            // Print the response body (String Text)
             System.out.println(response.body());
+
+            /* Data Provided for a certain date:
+            * 1. Date: YYYY-MM-DD [Format]
+            * 2. Symbol (Company Code): String; Example = AAPL [Apple]
+            * 3. Opening Stock (Corresponding Date): Double
+            * 4. Highest Stock (Corresponding Date): Double
+            * 5. Lowest Stock (Corresponding Date): Double
+            * 6. Close Stock (Corresponding Date): Double
+            * 7. Volume (Corresponding Date): Double
+            * */
 
         } catch (Exception e) {
             e.printStackTrace();

b1afcd4e6671f4c7dc6c06b7bef195d92e870072
Victor Gao <victorgao44@gmail.com>
2024-11-18 20:17:14 -0500

    Create CompareStocksPresenter presenter class.

---
+++ src/main/java/interface_adapters/compare_stocks/CompareStocksPresenter.java
@@ -0,0 +1,11 @@
+package main.java.interface_adapters.compare_stocks;
+
+import main.java.use_cases.compare_stocks.CompareStocksOutputBoundary;
+import main.java.use_cases.compare_stocks.CompareStocksOutputData;
+
+public class CompareStocksPresenter implements CompareStocksOutputBoundary {
+    @Override
+    public void displayResult(CompareStocksOutputData outputData) {
+
+    }
+}

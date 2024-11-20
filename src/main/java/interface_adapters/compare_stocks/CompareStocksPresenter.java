248408549f487c7df4c39512a76d2dc56910286e
Victor Gao <victorgao44@gmail.com>
2024-11-19 19:52:32 -0500

    Delete CompareStocksOutputData.java because the compare stocks use case only need to output a string and nothing else. So there is no need to have a separate data structure that only stores one string.
    Modify CompareStocksPresenter to implement new changes in CompareStocksOutputBoundary.

--- src/main/java/interface_adapters/compare_stocks/CompareStocksPresenter.java
+++ src/main/java/interface_adapters/compare_stocks/CompareStocksPresenter.java
@@ -1,11 +1,10 @@
 package main.java.interface_adapters.compare_stocks;
 
 import main.java.use_cases.compare_stocks.CompareStocksOutputBoundary;
-import main.java.use_cases.compare_stocks.CompareStocksOutputData;
 
 public class CompareStocksPresenter implements CompareStocksOutputBoundary {
     @Override
-    public void displayResult(CompareStocksOutputData outputData) {
+    public void displayComparisonSummary(String summary) {
 
     }
 }

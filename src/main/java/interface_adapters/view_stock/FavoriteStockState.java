2f464e2a8e4521b1ba2bba54591cbe6311eded58
perplop <andychen3870@gmail.com>
2024-11-26 23:34:27 -0500

    Finished FavoriteStockState

--- src/main/java/interface_adapters/view_stock/FavoriteStockState.java
+++ src/main/java/interface_adapters/view_stock/FavoriteStockState.java
@@ -1,4 +1,21 @@
 package interface_adapters.view_stock;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FavoriteStockState {
+    private Set<String> favoritedStocks = new HashSet<>();
+    private String error = null;
+
+    public Set<String> getFavoritedStocks() {
+        return new HashSet<>(favoritedStocks);
+    }
+
+    public void addFavorite(String symbol) {
+        favoritedStocks.add(symbol);
+    }
+
+    public void removeFavorite(String symbol) {
+        favoritedStocks.remove(symbol);
+    }
 }

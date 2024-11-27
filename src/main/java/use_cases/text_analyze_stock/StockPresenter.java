66a70d7a5ea8bc193321412996a94e49748232f8
unknown <you@example.com>
2024-11-27 16:09:20 -0500

    Updated Stock Presenter

--- src/main/java/use_cases/text_analyze_stock/StockPresenter.java
+++ src/main/java/use_cases/text_analyze_stock/StockPresenter.java
@@ -3,7 +3,8 @@ package use_cases.text_analyze_stock;
 class StockPresenter {
 
     public static void displayStockAnalysis(StockAnalysisResult result) {
-        System.out.println("The current price of stock " + result.getStockName() + " is " + result.getCurrentPrice() + ". \n"
+        System.out.println("The current price of stock " + result.getStockName() + " is " + result.getCurrentPrice() +
+                ". \n"
                 + " The projected price of the stock in 1 year is " + result.getProjectedPrice1() + ". \n"
                 + " The projected price of the stock in 2 years is " + result.getProjectedPrice2() + ". \n"
                 + " The projected price of the stock in 3 years is " + result.getProjectedPrice3() + ". \n"

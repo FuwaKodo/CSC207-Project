fb046b04b07eb270f660335e0f41f2e59b337458
perplop <andychen3870@gmail.com>
2024-11-26 23:35:47 -0500

    Finished FavoriteStock entity

--- src/main/java/entities/FavoriteStock.java
+++ src/main/java/entities/FavoriteStock.java
@@ -1,4 +1,19 @@
 package entities;
 
 public class FavoriteStock {
+    private final String symbol;
+    private final String company;
+
+    public FavoriteStock(String symbol, String company){
+        this.symbol = symbol;
+        this.company = company;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getCompany() {
+        return company;
+    }
 }

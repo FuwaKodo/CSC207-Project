0bd98879c44037d04632e14b4a57554f579afb4e
perplop <andychen3870@gmail.com>
2024-11-26 23:36:43 -0500

    updated ViewStockController

--- src/main/java/interface_adapters/view_stock/ViewStockController.java
+++ src/main/java/interface_adapters/view_stock/ViewStockController.java
@@ -2,16 +2,22 @@ package interface_adapters.view_stock;
 
 import use_cases.view_stock.ViewStockInputBoundary;
 import use_cases.view_stock.ViewStockInputData;
+import use_cases.favorites.FavoriteStockInputBoundary;
+import use_cases.favorites.FavoriteStockInputData;
 
 /**
- * Controller for the view stock use case.
+ * Controller for the view stock use case and favorites management.
  */
 public class ViewStockController {
 
     private final ViewStockInputBoundary viewStockUseCaseInteractor;
+    private final FavoriteStockInputBoundary favoriteStockUseCaseInteractor;
 
-    public ViewStockController(ViewStockInputBoundary viewStockUseCaseInteractor) {
+    public ViewStockController(
+            ViewStockInputBoundary viewStockUseCaseInteractor,
+            FavoriteStockInputBoundary favoriteStockUseCaseInteractor) {
         this.viewStockUseCaseInteractor = viewStockUseCaseInteractor;
+        this.favoriteStockUseCaseInteractor = favoriteStockUseCaseInteractor;
     }
 
     /**
@@ -20,7 +26,22 @@ public class ViewStockController {
      */
     public void execute(String stockSymbol) {
         final ViewStockInputData viewStockInputData = new ViewStockInputData(stockSymbol);
-
         viewStockUseCaseInteractor.execute(viewStockInputData);
+    }
+
+    /**
+     * Toggles the favorite status of a stock.
+     * @param stockSymbol the symbol of the stock to toggle
+     */
+    public void toggleFavorite(String stockSymbol) {
+        final FavoriteStockInputData favoriteStockInputData = new FavoriteStockInputData(stockSymbol);
+        favoriteStockUseCaseInteractor.toggleFavorite(favoriteStockInputData);
+    }
+
+    /**
+     * Retrieves the list of favorite stocks.
+     */
+    public void getFavorites() {
+        favoriteStockUseCaseInteractor.getFavorites();
     }
 }

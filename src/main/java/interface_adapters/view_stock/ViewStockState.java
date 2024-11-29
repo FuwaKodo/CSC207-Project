c59e1e41d437ec0040f69c09bed8d371f1d537a1
perplop <andychen3870@gmail.com>
2024-11-29 15:08:48 -0500

    updated ViewStockState readability + javadocs

--- src/main/java/interface_adapters/view_stock/ViewStockState.java
+++ src/main/java/interface_adapters/view_stock/ViewStockState.java
@@ -3,92 +3,161 @@ package interface_adapters.view_stock;
 import java.util.*;
 
 /**
- * The state for the Login View Model.
+ * Represents the state of the View Model for viewing stock information,
+ * including stock symbols, company names, share prices, earnings,
+ * errors, and favorite stocks.
  */
 public class ViewStockState {
+
+    /** The stock symbol being viewed. */
     private String symbol = "";
+
+    /** The company name associated with the stock symbol. */
     private String company = "";
+
+    /** A list of share prices for the stock over time. */
     private List<Double> sharePrices = new ArrayList<>();
+
+    /** A list of earnings for the stock over time. */
     private List<Double> earnings = new ArrayList<>();
+
+    /** An optional error message related to viewing stock information. */
     private String viewStockError;
+
+    /** A set of favorite stock symbols. */
     private Set<String> favorites = new HashSet<>();
 
+    /**
+     * Returns the current stock symbol.
+     *
+     * @return the stock symbol
+     */
     public String getSymbol() {
         return symbol;
     }
 
+    /**
+     * Returns the current company name.
+     *
+     * @return the company name
+     */
     public String getCompany() {
         return company;
     }
 
+    /**
+     * Returns the list of share prices.
+     *
+     * @return the list of share prices
+     */
     public List<Double> getSharePrices() {
         return sharePrices;
     }
 
     /**
-     * Return a list of share prices for the given days back in time.
-     * @param daysBack number of days to back track
-     * @return list of share prices in given time period
+     * Returns a list of share prices for the specified number of days back in time.
+     *
+     * @param daysBack the number of days to backtrack
+     * @return a list of share prices within the specified time period
      */
     public List<Double> getSharePrices(int daysBack) {
         return sharePrices.subList(sharePrices.size() - daysBack, sharePrices.size());
     }
 
+    /**
+     * Returns the list of earnings.
+     *
+     * @return the list of earnings
+     */
     public List<Double> getEarnings() {
         return earnings;
     }
 
     /**
-     * Return a list of earnings for the given days back in time.
-     * @param daysBack number of days to back track
-     * @return list of earnings in given time period
+     * Returns a list of earnings for the specified number of days back in time.
+     *
+     * @param daysBack the number of days to backtrack
+     * @return a list of earnings within the specified time period
      */
     public List<Double> getEarnings(int daysBack) {
         return earnings.subList(earnings.size() - daysBack, earnings.size());
     }
 
+    /**
+     * Returns the current view stock error message.
+     *
+     * @return the error message, or null if there is no error
+     */
     public String getViewStockError() {
         return viewStockError;
     }
 
+    /**
+     * Sets the stock symbol to be viewed.
+     *
+     * @param symbol the stock symbol to set
+     */
     public void setSymbol(String symbol) {
         this.symbol = symbol;
     }
 
+    /**
+     * Sets the company name associated with the stock symbol.
+     *
+     * @param company the company name to set
+     */
     public void setCompany(String company) {
         this.company = company;
     }
 
+    /**
+     * Sets the list of share prices.
+     *
+     * @param sharePrices the list of share prices to set
+     */
     public void setSharePrices(List<Double> sharePrices) {
         this.sharePrices = sharePrices;
     }
 
+    /**
+     * Sets the list of earnings.
+     *
+     * @param earnings the list of earnings to set
+     */
     public void setEarnings(List<Double> earnings) {
         this.earnings = earnings;
     }
 
+    /**
+     * Sets the view stock error message.
+     *
+     * @param symbolError the error message to set
+     */
     public void setViewStockError(String symbolError) {
         this.viewStockError = symbolError;
     }
 
     /**
-     * Add a stock symbol to favorites.
-     * @param symbol the stock symbol to add
+     * Adds a stock symbol to the favorites list.
+     *
+     * @param symbol the stock symbol to add to favorites
      */
     public void addFavorite(String symbol) {
         favorites.add(symbol);
     }
 
     /**
-     * Remove a stock symbol from favorites.
-     * @param symbol the stock symbol to remove
+     * Removes a stock symbol from the favorites list.
+     *
+     * @param symbol the stock symbol to remove from favorites
      */
     public void removeFavorite(String symbol) {
         favorites.remove(symbol);
     }
 
     /**
-     * Check if a stock symbol is favorited.
+     * Checks if a stock symbol is in the favorites list.
+     *
      * @param symbol the stock symbol to check
      * @return true if the stock is favorited, false otherwise
      */
@@ -97,7 +166,8 @@ public class ViewStockState {
     }
 
     /**
-     * Get all favorite stock symbols.
+     * Returns an unmodifiable set of all favorite stock symbols.
+     *
      * @return an unmodifiable set of favorite stock symbols
      */
     public Set<String> getFavorites() {

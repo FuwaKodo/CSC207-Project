ec341a3bf3f55c8dc67d4b2bb1f1ac7e1e13ce1c
perplop <andychen3870@gmail.com>
2024-11-30 00:16:35 -0500

    Created FavoriteStock Presenter and State

--- src/main/java/use_cases/favorites/FavoriteStockOutputBoundary.java
+++ src/main/java/use_cases/favorites/FavoriteStockOutputBoundary.java
@@ -4,28 +4,20 @@ import java.util.Set;
 
 /**
  * Output boundary interface for favorite stock operations.
- * This interface defines the contract for presenting the results
- * of favorite stock operations to the user interface layer.
  */
 public interface FavoriteStockOutputBoundary {
-
     /**
-     * Presents the result of a favorite toggle operation.
-     * This method should be called after a stock's favorite status has been changed.
+     * Called when a stock's favorite status is toggled.
      *
-     * @param symbol      The stock symbol that was toggled
-     * @param isFavorited The new favorite status of the stock:
-     *                    true if the stock was added to favorites,
-     *                    false if it was removed
+     * @param symbol The stock symbol that was toggled
+     * @param isFavorited The new favorite status of the stock
      */
     void presentFavoriteToggled(String symbol, boolean isFavorited);
 
     /**
-     * Presents the complete list of favorite stocks.
-     * This method should be called in response to a request for all favorite stocks.
+     * Called to present a complete set of favorite stocks.
      *
-     * @param favorites A set of stock symbols representing all currently
-     *                 favorited stocks. An empty set indicates no favorites.
+     * @param favorites The set of all favorited stock symbols
      */
     void presentFavorites(Set<String> favorites);
 }
